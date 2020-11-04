ac3ce11644f8b51f2800c4d777b1dc5f73cea868
oliviercros75 <58745704+oliviercros75@users.noreply.github.com>
2020-11-04 18:59:28 +0100

    myBlog content
    Sending the code to the repo

---
+++ blog/forms.py
@@ -0,0 +1,18 @@
+from .models import Comment, Post
+from django import forms
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title']
+
+class PostDeleteForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = []
